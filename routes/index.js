953c52ee0b9311a531c951250fc9e288bfa98df8
fenil jariwala <feniljaiwala82@gmail.com>
2020-04-08 21:29:07 +0530

    Minor corrections

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Contact-Keeper-Server' });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Contact-Keeper-Server' });
+// });
 
 module.exports = router;
